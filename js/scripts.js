0e40214dc98a42c3bfb1d4fa9ee8845aca099fc9
Joseph Pearce <joseph@zotta.com>
2020-04-07 16:15:31 -0700

    Add ticket business logic and HTML forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // need a constructor for movie ticket (movieName --> movieType, age --> ageType, time --> timeRange)
 // want prototype function for movie type (first release, normal release)
 // want prototype function for determining age (regular price, senior price)
-// want prototype function for time (9:00-12:00, 13:00-17:00, 18:00-22:00)
+// want prototype function for time 
 //Your constructor and prototype could be called Ticket and you can come up with the formula for determining how the price is calculated depending on the input from the user.
 
 // Business Logic for Theatre Ticket
@@ -11,37 +11,55 @@ function TheatreTicket( ) {
   this.currentId = 0;
 }
 
+
+
 // Business logic for Ticket
 
-function Ticket(firstRelease, regularRelease, regularPrice, seniorPrice, matineeTime, regularTime) {
-  this.firstRelease = firstRelease;
-  this.regularRelease = regularRelease;
-  this.regularPrice = regularPrice;
-  this.seniorPrice = seniorPrice;
-  this.matineeTime = matineeTime;
-  this.regularTime = regularTime; 
+function Ticket(movieInput, timeInput, ageInput) {
+  this.movieInput = movieInput;
+  this.timeInput = timeInput;
+  this.ageInput = ageInput; 
 }
 
-Ticket.prototype.releaseType = function() {
-  return this.firstRelease + " " + this.regularRelease;
+Ticket.prototype.releaseType = function(t) {
+  if(t === "king-kong") {
+    return "firstRelease"
+  } else if(t === "hausu") {
+    return "regularRelease"
+  } else if(t === "2001") {
+    return "regularRelease"
+  } else if(t === "nobody") {
+    return "firstRelease"
+  } else {
+    console.log("there is an error in release type.")
+  }
 }
 
-Ticket.prototype.priceType = function()
+Ticket.prototype.priceType = function() {
   return this.regularPrice + " " + this.seniorPrice;
+}
+  
 
-Ticket.prototype.timeType = function()
+Ticket.prototype.timeType = function() {
   return this.matineeTime + " " + this.regularTime
+}
 
 
+  
+$(doctype).ready(function() {
+  $("form#ticketInput").submit(function(event) {
+    event.preventDefault();
+    var inputtedMovie = $("#movieInput input[type='radio']:checked").val();
+    var inputtedTime = $("#timeInput input[type='radio']:checked").val();
+    var inputtedAge = $("input#age").val();
+    var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
+    TheatreTicket.addTicket(newTicket);
+    console.log("this is the something");
+  
 
+  
 
-// function Movie(firstRelease, matinee, ) {
-//   this.movieType = movieType;
-//   this.ageType = ageType;
-//   this.timeRange = timeRange;
-// }
-
-// Movie.prototype.movie = function() {
-//   return this.movieType + " " + this.ageType 
-// }
+  });
+  
+});
 
