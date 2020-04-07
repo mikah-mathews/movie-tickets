1df20c6f0742de8e7b1f65c1fdcb81368cad7b1d
Joseph Pearce <joseph@zotta.com>
2020-04-07 16:36:29 -0700

    Add UI logic, add aditional business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
-// need a constructor for movie ticket (movieName --> movieType, age --> ageType, time --> timeRange)
-// want prototype function for movie type (first release, normal release)
-// want prototype function for determining age (regular price, senior price)
-// want prototype function for time 
-//Your constructor and prototype could be called Ticket and you can come up with the formula for determining how the price is calculated depending on the input from the user.
+// TODO
+// get radio button ID to compare for IF statements
+// make function to calculate prices
 
 // Business Logic for Theatre Ticket
+
+
 
 function TheatreTicket( ) {
   this.tickets = [];
   this.currentId = 0;
 }
+
+TheatreTicket.prototype.addTicket = function(ticket) {
+  ticket.id = this.assignId();
+  this.tickets.push(ticket);
+}
+
+TheatreTicket.prototype.assignId = function() {
+  this.currentId +=1;
+  return this.currentId;
+}
+
+// User Interface Logic
+
+var theatreTicket = new TheatreTicket();
+
 
 
 
@@ -31,35 +46,41 @@ Ticket.prototype.releaseType = function(t) {
   } else if(t === "nobody") {
     return "firstRelease"
   } else {
-    console.log("there is an error in release type.")
+    console.log("there is an error in release type.");
   }
 }
 
-Ticket.prototype.priceType = function() {
-  return this.regularPrice + " " + this.seniorPrice;
-}
+Ticket.prototype.timeType = function(t) {
+  if(t === "matinee") {
+    return "matineeRelease";
+  } else if(t === "regularRelease") {
+    return "regularRelease";
+  } else {
+    console.log("there is an error in timeType.");
+  }
+}; 
+
+// Ticket.prototype.timeType = function() {
+//   return this.matineeTime + " " + this.regularTime
+// }
+
+
+
+
   
-
-Ticket.prototype.timeType = function() {
-  return this.matineeTime + " " + this.regularTime
-}
-
-
-  
-$(doctype).ready(function() {
+$(document).ready(function() {
   $("form#ticketInput").submit(function(event) {
     event.preventDefault();
     var inputtedMovie = $("#movieInput input[type='radio']:checked").val();
     var inputtedTime = $("#timeInput input[type='radio']:checked").val();
     var inputtedAge = $("input#age").val();
     var newTicket = new Ticket(inputtedMovie, inputtedTime, inputtedAge);
-    TheatreTicket.addTicket(newTicket);
+    console.log(newTicket);
+    theatreTicket.addTicket(newTicket);
     console.log("this is the something");
   
 
-  
 
   });
-  
 });
 
